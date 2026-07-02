ba89bb500e073656cac28f133edd0e3ee1bac001
raquellzz <raquelcostafreire24@gmail.com>
2026-07-02 11:22:45 -0300

    add: Criação do trustcore para outras instâncias e outras adaptações

---
+++ backend/src/main/java/imd/ufrn/com/br/smart_space_booking/framework/exception/RecursoNotFoundException.java
@@ -0,0 +1,7 @@
+package imd.ufrn.com.br.smart_space_booking.framework.exception;
+
+public class RecursoNotFoundException extends RuntimeException {
+    public RecursoNotFoundException(String mensagem) {
+        super(mensagem);
+    }
+}
